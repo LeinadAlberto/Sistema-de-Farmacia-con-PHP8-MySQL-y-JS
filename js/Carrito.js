95eac2d35660bb00ef5ef7628601d767929ff7d7
Alberto <dany.canaviri2022@gmail.com>
2023-08-19 22:37:28 -0400

    Carrito de Compras con Local Storage Finalizado

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+    RecuperarLS_carrito();
+
     $(document).on('click', '.agregar-carrito', (e) => {
         const elemento = $(this)[0].activeElement.parentElement.parentElement.parentElement.parentElement;
         const id = $(elemento).attr('prodId');
@@ -24,25 +27,96 @@ $(document).ready(function() {
             cantidad: 1
         };
 
-        template = `
-            <tr>
-                <td>${producto.id}</td>
-                <td>${producto.nombre}</td>
-                <td>${producto.concentracion}</td>
-                <td>${producto.adicional}</td>
-                <td>${producto.precio}</td>
-                <td><button class="borrar-producto btn btn-danger"><i class="fas fa-times-circle"></i></button></td>
-            </tr>
-        `;
-        $('#lista').append(template);
+        let id_producto;
+        let productos;
+        productos = RecuperarLS(); /* Retorna un Array de Objetos JS */
+        productos.forEach(prod => {
+            if (prod.id === producto.id) {
+                id_producto = prod.id;
+            }
+        });
+        if (id_producto === producto.id) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Recuerda...',
+                text: 'Ya enviaste al carrito este producto!',
+            });
+        } else {
+            template = `
+                <tr prodId="${producto.id}">
+                    <td>${producto.id}</td>
+                    <td>${producto.nombre}</td>
+                    <td>${producto.concentracion}</td>
+                    <td>${producto.adicional}</td>
+                    <td>${producto.precio}</td>
+                    <td><button class="borrar-producto btn btn-danger"><i class="fas fa-times-circle"></i></button></td>
+                </tr>
+            `;
+            $('#lista').append(template);
+            /* Función que permitira agregar un producto al Local Storage */
+            AgregarLS(producto);
+        }
     });
 
     $(document).on('click', '.borrar-producto', (e) => {
         const elemento = $(this)[0].activeElement.parentElement.parentElement;
+        const id = $(elemento).attr('prodId');
         elemento.remove();
+        Eliminar_producto_LS(id);
     });
 
     $(document).on('click', '#vaciar-carrito', (e) => {
         $('#lista').empty();
+        EliminarLS();
     });
+
+    function RecuperarLS() {
+        let productos;
+        if (localStorage.getItem('productos') === null) {
+            productos = [];
+        } else {
+            productos = JSON.parse(localStorage.getItem('productos'));
+        }
+        return productos;
+    }
+
+    function AgregarLS(producto) {
+        let productos;
+        productos = RecuperarLS();
+        productos.push(producto);
+        localStorage.setItem('productos', JSON.stringify(productos));
+    }
+
+    function RecuperarLS_carrito() {
+        let productos;
+        productos = RecuperarLS();
+        productos.forEach(producto => {
+            template = `
+                <tr prodId="${producto.id}">
+                    <td>${producto.id}</td>
+                    <td>${producto.nombre}</td>
+                    <td>${producto.concentracion}</td>
+                    <td>${producto.adicional}</td>
+                    <td>${producto.precio}</td>
+                    <td><button class="borrar-producto btn btn-danger"><i class="fas fa-times-circle"></i></button></td>
+                </tr>
+            `;
+            $('#lista').append(template);
+        });
+    }
+
+    function Eliminar_producto_LS(id) {
+        let productos;
+        productos = RecuperarLS();
+        productos.forEach(function(producto, indice){
+            if (producto.id === id) {
+                productos.splice(indice,1);
+            }
+        });
+        localStorage.setItem('productos', JSON.stringify(productos));
+    }
+
+    function EliminarLS() {
+        localStorage.clear();
+    }
 });
